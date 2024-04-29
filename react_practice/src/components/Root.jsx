6f1e19acb49b2050c9f189b0f78d2752289317a5
dk10119 <tranduclong092@gmail.com>
2024-04-29 13:13:21 +0300

    Refactor: move header, footer to root component, add outlet component from react router dom

--- react_practice/src/components/Root.jsx
+++ react_practice/src/components/Root.jsx
@@ -1,13 +1,13 @@
-import Footer from './Footer';
-import Header from './components/Header';
+import Header from "./Header";
+import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
+import '../App.css'
 
 export default function Root() {
   return (
     <>
       <Header />
-      <main>
-        <p>Content</p>
-      </main>
+      <Outlet />
       <Footer />
     </>
   )
