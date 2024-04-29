82d366d8ae86132e4f3eccdae1b7e1008308d75d
dk10119 <tranduclong092@gmail.com>
2024-04-29 13:16:08 +0300

    Fix: fix footer not at bottom for page with height less than 100vh

--- react_practice/src/components/Footer.jsx
+++ react_practice/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="bg-blue-400 bg-opacity-55 p-6">
-        <p>Copyright &copy; Alex Tran</p>
+    <footer className="bg-blue-400 bg-opacity-55 p-6 sticky top-[100vh] m-auto text-center">
+      <p>Copyright &copy; Alex Tran</p>
     </footer>
   )
 }
