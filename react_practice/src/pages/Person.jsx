bcc7c5b9b99ace12ccd4a4c0bbbc1adcd994bdf7
dk10119 <tranduclong092@gmail.com>
2024-05-13 14:03:03 +0300

    Feat: practice axios and useEffect by fetching users from api

--- react_practice/src/pages/Person.jsx
+++ react_practice/src/pages/Person.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+import Box from "../components/Box";
+
 export default function Person() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(response => setUsers(response.data))
+      .catch(error => console.log(error))
+  }, [])
+
+  // console.log(users);
+
   return (
-    <div>Person</div>
+    <div>
+      {users.map(user =>
+        <div key={user.id}>
+          <Box name={user.name} title={user.company.name} location={user.address.city} />
+        </div>
+      )}
+    </div>
   )
 }
