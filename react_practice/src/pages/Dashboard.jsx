c964f5d8fcf0c7dab9c6d45d69a7cc1512d0f9de
dk10119 <tranduclong092@gmail.com>
2024-05-13 14:31:59 +0300

    Build + feat: install json server, create and host  a fake db and display data

--- react_practice/src/pages/Dashboard.jsx
+++ react_practice/src/pages/Dashboard.jsx
@@ -1,10 +1,34 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 export default function Dashboard({ user }) {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => { axios.get("http://localhost:3000/posts").then(res => setPosts(res.data)).catch(error => console.log(error)) }, [])
+
+  // need work: handle publish or not button
+
   return (
-    <div>
-      {user === 'Alex'
-        ? (<h1>Welcome to dashboard, {user}</h1>)
-        : (<h1>Welcome to dashboard guest</h1>)
-      }
-    </div>
+    <>
+      <div>
+        {user === 'Alex'
+          ? (<h1>Welcome to dashboard, {user}</h1>)
+          : (<h1>Welcome to dashboard guest</h1>)
+        }
+      </div>
+      <div className="p-5">
+        <ul className="grid grid-cols-2 gap-4">
+          {posts.map(post =>
+            <li key={post.id} className="text-start p-7 border-2 rounded shadow">
+              <p>Title: {post.title}</p>
+              <p>Author: {post.author}</p>
+              <p>Date: {post.date}</p>
+              <p>Content: {post.content}</p>
+              <button className="bg-white p-3 rounded shadow">Published: <span>{post.published ? "Yes" : "No"}</span></button>
+            </li>
+          )}
+        </ul>
+      </div>
+    </>
   )
 }
